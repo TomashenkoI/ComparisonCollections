070b9f6067d362088bb688b7743ee7ce292729ea
snitelol <TomashenkoI@gmail.com>
2016-07-19 10:53:53 +0300

    Initial commit

---
+++ src/Main/HashSet.java
@@ -0,0 +1,17 @@
+package Main;
+
+/**
+ * Created by 7 on 18.07.2016.
+ */
+public class HashSet {
+
+    public static String setType = "Hash";
+    public static java.util.HashSet<Integer> hashSet = new java.util.HashSet<>();
+    public static ImplementationForEachSetCase implementationForEachSetCase = new ImplementationForEachSetCase();
+
+    public static void run(int count, String volume) {
+
+        implementationForEachSetCase.implementation(setType ,volume, count, hashSet);
+
+    }
+}
